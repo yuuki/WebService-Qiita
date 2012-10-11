485b19cfd8a786614acb1f138d4dd9b7457be41d
y_uuki <yuki.tsubo@gmail.com>
2012-10-11 16:26:05 +0900

    initial commit

---
+++ lib/Net/Qiita.pm
@@ -0,0 +1,32 @@
+package Net::Qiita;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Net::Qiita -
+
+=head1 SYNOPSIS
+
+  use Net::Qiita;
+
+=head1 DESCRIPTION
+
+Net::Qiita is
+
+=head1 AUTHOR
+
+Yuuki Tsubouchi E<lt>yuki.tsubo@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
