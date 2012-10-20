035a02e311bb50c147ff5b2c7440ebb236eb27d0
y_uuki <yuki.tsubo@gmail.com>
2012-10-20 16:00:06 +0900

    add user command

--- lib/WebService/Qiita/CLI.pm
+++ lib/WebService/Qiita/CLI.pm
@@ -52,6 +52,10 @@ sub parse_options {
             me => {
                 desc => 'Shows your info',
             },
+            user => {
+                args => 'username',
+                desc => 'Shows user info',
+            },
         },
     );
 
@@ -77,10 +81,19 @@ sub run {
 
     my $command = $self->{command} or diag('no specified subcommand') and return 1;
     if ($command eq 'me') {
-        $self->dump_me;
+        my $me = $self->{client}->user;
+        $self->dump_user($me);
+    }
+    elsif ($command eq 'user') {
+        my $url_name = shift @ARGV;
+        my $user = $self->{client}->user($url_name);
+        $self->dump_user($user);
     }
     elsif ($command eq 'search') {
         $self->dump_search(join ' ', @ARGV);
+    }
+    else {
+        diag("no such command $command") and return 1;
     }
 
     return 0;
@@ -143,30 +156,29 @@ sub filter_utf8 {
     }
 }
 
-sub dump_me {
-    my $self = shift;
+sub dump_user {
+    my ($self, $user) = @_;
 
-    my $me = $self->{client}->user;
-    filter_utf8($me);
+    filter_utf8($user);
 
-    print <<ME;
-name:  $me->{name}
-id:    $me->{url_name}
-URL:   $me->{url}
+    print <<USER;
+user:  $user->{name}
+id:    $user->{url_name}
+URL:   $user->{url}
 Description:
-$me->{description}
-WEB site URL: $me->{website_url}
-Organization: $me->{organization}
-location:     $me->{location}
-followers:       $me->{followers}
-following_users: $me->{following_users}
-items:           $me->{items}
+$user->{description}
+WEB site URL: $user->{website_url}
+Organization: $user->{organization}
+location:     $user->{location}
+followers:       $user->{followers}
+following_users: $user->{following_users}
+items:           $user->{items}
 
-Twitter:  $me->{twitter}
-GitHub:   $me->{github}
-Facebook: $me->{facebook}
-Linkedin: $me->{linkedin}
-ME
+Twitter:  $user->{twitter}
+GitHub:   $user->{github}
+Facebook: $user->{facebook}
+Linkedin: $user->{linkedin}
+USER
     return 1;
 }
 
